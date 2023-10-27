a5f1851dba2ee3f4e01f1ef2737334dad2741a81
samyam5032 <124229986+samyam5032@users.noreply.github.com>
2023-10-27 07:59:58 -0400

    Update Header.js

--- LAF/src/components/Header.js
+++ LAF/src/components/Header.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';  
 
-function Header() {
+function Header({ onMenuPress }) {
     return (
-        <ImageBackground 
-            source={require('../../assets/images/logo.png')} 
-            style={styles.header}>
-            <Text style={styles.headerText}>LOST & FOUND</Text>
-        </ImageBackground>
+        <View style={styles.container}>
+            <TouchableOpacity onPress={onMenuPress} style={styles.menuIcon}>
+                <Ionicons name="menu" size={30} color="white" />
+            </TouchableOpacity>
+            <ImageBackground 
+                source={require('../../assets/images/logo.png')} 
+                style={styles.header}
+                resizeMode="contain"
+            />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: 200,
+    container: {
+        width: '100%',
+        height: 100,   
+        backgroundColor: '#333', 
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'row',
+        paddingTop: 90, 
+    },
+    header: {
+        flex: 1,
+        height: 60, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,  
+    },
+    menuIcon: {
+        marginHorizontal: 25,  
     },
     headerText: {
         color: 'white',
