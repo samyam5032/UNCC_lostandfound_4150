73c9f67642b39dd5ea7c4165380d67e20e5f9191
samyam5032 <slamich2@uncc.edu>
2023-11-05 16:45:03 -0500

    location map updated

--- LAF/src/screens/LocationScreen/LocationScreen.js
+++ LAF/src/screens/LocationScreen/LocationScreen.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 const LocationScreen = () => {
+  const initialRegion = {
+    latitude: 35.30881988,
+    longitude: -80.73374287,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
   return (
     <View style={styles.container}>
+      {/* Map Section */}
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 35.3075,
-          longitude: -80.7352,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+        initialRegion={initialRegion}
+      >
+        <Marker
+          coordinate={initialRegion}
+          pinColor="red"
+          title="Pop Martin Student Union"
+        />
+      </MapView>
+
+      {/* Description Section */}
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.titleText}>
+          Pop Martin Student Union
+        </Text>
+        <Text style={styles.descriptionText}>
+          Monday - Friday: 7AM - 12AM{'\n'}
+          Saturday - Sunday: 9AM - 12AM{'\n'}
+          Contact Number: 
+        </Text>
+      </View>
     </View>
   );
 };
@@ -21,12 +42,25 @@ const LocationScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+  descriptionContainer: {
+    flex: 1,
+    padding: 16,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  map: {
-    width: '100%',
-    height: '100%',
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 8, 
+  },
+  descriptionText: {
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
 
