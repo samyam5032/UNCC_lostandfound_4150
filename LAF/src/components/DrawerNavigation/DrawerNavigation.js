bb80d7a126a2e954504504667e15a2e59113cb92
michelynodei1 <michelynodei1@gmail.com>
2023-11-30 13:49:28 -0500

    Updated The App
    Updated the app so it all up to date except for admin

--- LAF/src/components/DrawerNavigation/DrawerNavigation.js
+++ LAF/src/components/DrawerNavigation/DrawerNavigation.js
@@ -6,7 +6,7 @@ import HomePageScreen from '../../screens/HomePageScreen';
 import SignInScreen from '../../screens/SignInScreen';
 import FAQScreen from '../../screens/FAQScreen';
 import LocationScreen from '../../screens/LocationScreen';
-import ProfileScreen from '../../screens/ProfileScreen';     
+import ProfileScreen from '../../screens/ProfileScreen';    
 import SettingScreen from '../../screens/SettingScreen';
 import LandingScreen from '../../screens/LandingScreen';
 import LogoutScreen from '../../screens/LogoutScreen';
@@ -17,30 +17,37 @@ import RegisterScreen from '../../screens/RegisterScreen';
 import SignInScreenNew from '../../screens/SignInScreenNew';
 
 
+
+
 const Drawer = createDrawerNavigator();
 
+
 const DrawerNavigation = () => {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomePageScreen} />
-        <Drawer.Screen name="My Profile" component={ProfileScreen} />
-        <Drawer.Screen name="Location" component={LocationScreen} />
-        <Drawer.Screen name="FAQ" component={FAQScreen} />
-        <Drawer.Screen name="Setting" component={SettingScreen} />
-        <Drawer.Screen name="Landing Page" component={LandingScreen} />
-        <Drawer.Screen name="Logout" component={LogoutScreen} />
-        <Drawer.Screen name="Edit Profile" component={EditProfileScreen} />
-        <Drawer.Screen name="Sign In" component={SignInScreen}/>
-        <Drawer.Screen name="Create Account" component={CreateAccountScreen}/>
-        <Drawer.Screen name="Forgot Password" component={ForgotPasswordScreen}/>
-        <Drawer.Screen name="Register" component={RegisterScreen}/>
-        <Drawer.Screen name="Sign In New" component={SignInScreenNew}/>
-      </Drawer.Navigator>
-      
-      
-      </NavigationContainer>
-  );
+ return (
+   <NavigationContainer>
+     <Drawer.Navigator initialRouteName="Home">
+       <Drawer.Screen name="Home" component={HomePageScreen} />
+       <Drawer.Screen name="My Profile" component={ProfileScreen} />
+       <Drawer.Screen name="Location" component={LocationScreen} />
+       <Drawer.Screen name="FAQ" component={FAQScreen} />
+       <Drawer.Screen name="Setting" component={SettingScreen} />
+       <Drawer.Screen name="Landing Page" component={LandingScreen} />
+       <Drawer.Screen name="Logout" component={LogoutScreen} />
+       <Drawer.Screen name="Edit Profile" component={EditProfileScreen} />
+       <Drawer.Screen name="Forgot Password" component={ForgotPasswordScreen}/>
+       <Drawer.Screen name="Register" component={RegisterScreen}/>
+       <Drawer.Screen name="Sign In New" component={SignInScreenNew}/>
+     </Drawer.Navigator>
+     </NavigationContainer>
+
+
+     /*
+       * Old code for old screens
+       <Drawer.Screen name="Sign In" component={SignInScreen}/>
+       <Drawer.Screen name="Create Account" component={CreateAccountScreen}/>
+     */
+ );
 };
- 
+
+
 export default DrawerNavigation;
